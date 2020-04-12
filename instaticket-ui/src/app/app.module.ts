4ad712f74007bd7630043d51e9b5bd15a696b830
shilpasandeep <shilpanair92@gmail.com>
2020-04-12 23:56:44 +0530

    Login Form added

--- instaticket-ui/src/app/app.module.ts
+++ instaticket-ui/src/app/app.module.ts
@@ -2,19 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+
     LoginComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatIconModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- instaticket-ui/src/app/login/login.component.ts
+++ instaticket-ui/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DataServiceService } from '../data-service.service';
+
 
 @Component({
   selector: 'app-login',
@@ -7,15 +10,42 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   height: any = 500;
+  loginForm: FormGroup;
+
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     this.height = window.innerHeight;
   }
-  constructor() { }
+  constructor(private fb: FormBuilder, private dataService: DataServiceService) { }
 
   ngOnInit() {
     this.height = window.innerHeight;
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+  onSubmit() {
+
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    var user = { username: this.f.username.value, password: this.f.password.value };
+
+    //console.log(this.f.username.value);
+
+
+    this.dataService.login(user).subscribe(data => {
+      console.log(data)
+    })
+    console.log("Form Submitted")
   }
 
 }
